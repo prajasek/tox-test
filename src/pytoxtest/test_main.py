46452db13e3e9d92cc0f9d9f7d336013fcdee965
Prasanth <sekaranp92@gmail.com>
2024-09-27 01:02:58 +0000

    add fixtures

--- src/pytoxtest/test_main.py
+++ src/pytoxtest/test_main.py
@@ -11,3 +11,17 @@ def test_add2():
 def test_add3():
     with pytest.raises(Exception):
        add(1+ "")
+
+@pytest.fixture
+def values_fix():
+    return [1,2,3]
+
+@pytest.fixture
+def test_values(values_fix):
+    values_fix.append(4)
+    assert [1,2,3,4] == values_fix
+    return values_fix
+
+def test_values1(test_values, values_fix):
+    values_fix.append(5)
+    assert [1,2,3,4,5] == values_fix
